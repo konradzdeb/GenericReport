37f8fa9bcc9b02076605d989a634d39060d84edf
Konrad Zdeb <konrad.zdeb@me.com>
2021-11-13 12:19:54 +0000

    Code listing
    Signed-off-by: Konrad Zdeb <konrad.zdeb@me.com>

---
+++ exampleClass.java
@@ -0,0 +1,17 @@
+/*
+ * exampleClass.java
+ * Sample Java class
+ *
+ */
+
+public class Class_Example  {
+    public Class_Example() {
+        String value = "Test";// Local Variable inside a Constructor
+    }
+
+    public void disp() {
+        String name = "JavaInterviewPoint";// Local variable inside method
+        System.out.println("Welcome " + name + "!!!");
+    }
+}
+
